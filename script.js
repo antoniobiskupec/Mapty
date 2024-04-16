38b1cd2e8a484da58128d5fe53ce0eb8156addd7
antonio <antoniobiskupec1@gmail.com>
2024-04-16 20:39:24 +0200

    inputs throw an error if we send anything else other than number and if that number is negative

--- script.js
+++ script.js
@@ -15,6 +15,46 @@ const months = [
   "December",
 ];
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  contructor(coords, distance, duration) {
+    this.coords = coords; // lat,lng
+    this.distance = distance; // in km
+    this.duration = duration; // in minutes
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+//////////////////////////////////////////////////////////
+// App architecture
+
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -76,10 +116,51 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every((inp) => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
+
     e.preventDefault();
+
+    // get data from a form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    // if acticity running, create running object
+    if (type === "running") {
+      const cadence = +inputCadence.value;
+      // check if data is valid
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert("Inputs have to be positive number");
+    }
+
+    // if activity cycling, crate cycling object
+
+    if (type === "cycling") {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert("Inputs have to be positive number");
+    }
+
+    // add new object to workout array
+
+    // render workout on map as marker
+
+    // render workour on list
+
+    // hide form = clear input fields
 
     // Clear input fields
     inputElevation.value = "";
+
     inputDistance.value = "";
     inputDuration.value = "";
     inputCadence.value = "";
